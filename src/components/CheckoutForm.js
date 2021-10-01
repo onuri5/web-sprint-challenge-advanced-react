7700de795ca87d95422fb42e6550c0ab782d39c0
onuri5 <jomibaofficial@gmail.com>
2021-10-01 12:43:50 -0500

    answered readme questions

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -14,7 +14,7 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
+const CheckoutForm = () => {
   const [showSuccessMessage, values, handleChanges, handleSubmit] = useForm(initialValue);
 
   return (
